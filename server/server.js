d1fef59995fc5ce722c7f813eec57e4e64084918
Riccardorossi23 <55913260+Riccardorossi23@users.noreply.github.com>
2026-09-04 11:49:51 +0200

    Aggiunge supporto SSL per Aiven

--- server/server.js
+++ server/server.js
@@ -14,7 +14,8 @@ const db = mysql.createConnection({
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'f1'
+    database: process.env.DB_NAME || 'f1',
+    ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : undefined
 });
 
 db.connect(err => {
